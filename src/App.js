c2087064d9a4824f1e311be118a9aa11d89bd5eb
nyius <josephdesigned@gmail.com>
2022-04-23 15:56:47 -0400

    Added first login/account creation functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import AllQuizes from './pages/AllQuizes';
+import MyProfile from './pages/MyProfile';
+import CreateQuiz from './pages/CreateQuiz';
+import CreateAccount from './pages/CreateAccount';
+import Quiz from './pages/Quiz';
+import UserProfile from './pages/UserProfile';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import PrivateRoutes from './components/PrivateRoutes';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="flex flex-col relative h-screen bg-base-100 mainApp">
+				<Navbar />
+				<div className="flex flex-col mx-auto h-screen w-11/12 max-w-6xl justify-center items-center">
+					<Routes>
+						<Route exact path="/" element={<Login />} />
+						<Route exact path="/quizzes" element={<AllQuizes />} />
+						<Route exact path="/quiz/:id" element={<Quiz />} />
+						<Route exact path="/profile/:userId" element={<UserProfile />} />
+						<Route exact path="/create-account" element={<CreateAccount />} />
+						<Route
+							exact
+							path="/my-profile"
+							element={
+								<PrivateRoutes>
+									<MyProfile />
+								</PrivateRoutes>
+							}
+						/>
+						<Route
+							exact
+							path="/create-quiz"
+							element={
+								<PrivateRoutes>
+									<CreateQuiz />
+								</PrivateRoutes>
+							}
+						/>
+					</Routes>
+				</div>
+				<ToastContainer />
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
